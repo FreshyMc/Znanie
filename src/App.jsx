7d952480340354e4f9b7025e318c59fd42bfc815
martoo3939 <167322682+martoo3939@users.noreply.github.com>
2024-12-01 19:51:15 +0200

    Add changes to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,76 @@
-import { useState } from 'react'
+import { useId, useState } from 'react'
 import Logo from './assets/logo.png';
 import { Link } from 'react-router-dom';
+
+import Slide1 from './assets/kids_1.jpg';
+import Slide2 from './assets/kids_2.jpg';
+import Slide3 from './assets/kids_3.jpg';
+import Slide4 from './assets/kids_4.jpg';
+import Slide5 from './assets/kids_5.jpg';
+import Slide6 from './assets/kids_6.jpg';
+import Slide7 from './assets/kids_7.jpg';
+
+import Avatar1 from './assets/avatar_1.jfif';
+import Avatar2 from './assets/avatar_2.jfif';
+import Avatar3 from './assets/avatar_3.jfif';
+import Avatar4 from './assets/avatar_4.jfif';
 
 const slides = [
   {
     name: 'Borko',
+    image: Avatar1,
     grade: 1,
     comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et quibusdam deleniti repudiandae dignissimos dolor deserunt vitae placeat! Vitae, doloribus.'
   },
   {
     name: 'Niki',
+    image: Avatar2,
     grade: 6,
     comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et quibusdam deleniti repudiandae dignissimos dolor deserunt vitae placeat! Vitae, doloribus.'
   },
   {
     name: 'Alex',
+    image: Avatar3,
     grade: 10,
     comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et quibusdam deleniti repudiandae dignissimos dolor deserunt vitae placeat! Vitae, doloribus.'
+  },
+  {
+    name: 'Diana',
+    image: Avatar4,
+    grade: 11,
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et quibusdam deleniti repudiandae dignissimos dolor deserunt vitae placeat! Vitae, doloribus.'
   }
+];
+
+const headerSlides = [
+  {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem cumque, labore fugit corrupti vitae earum dolor repudiandae quidem, laborum alias est. Rem soluta quaerat sint accusantium molestias cum, earum officiis.',
+    image: Slide1
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem cumque, labore fugit corrupti vitae earum dolor repudiandae quidem, laborum alias est. Rem soluta quaerat sint accusantium molestias cum, earum officiis.',
+    image: Slide2
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem cumque, labore fugit corrupti vitae earum dolor repudiandae quidem, laborum alias est. Rem soluta quaerat sint accusantium molestias cum, earum officiis.',
+    image: Slide3
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem cumque, labore fugit corrupti vitae earum dolor repudiandae quidem, laborum alias est. Rem soluta quaerat sint accusantium molestias cum, earum officiis.',
+    image: Slide4
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem cumque, labore fugit corrupti vitae earum dolor repudiandae quidem, laborum alias est. Rem soluta quaerat sint accusantium molestias cum, earum officiis.',
+    image: Slide5
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem cumque, labore fugit corrupti vitae earum dolor repudiandae quidem, laborum alias est. Rem soluta quaerat sint accusantium molestias cum, earum officiis.',
+    image: Slide6
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem cumque, labore fugit corrupti vitae earum dolor repudiandae quidem, laborum alias est. Rem soluta quaerat sint accusantium molestias cum, earum officiis.',
+    image: Slide7
+  },
 ];
 
 function App() {
@@ -97,13 +150,8 @@ function App() {
         </div>
       </nav>
       <header className='row main-header'>
-        <div className='col-12'>
-          <div className='header-content'>
-            <h1 className='pt-4 mt-4'>Znanie</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus mollitia, impedit corrupti perferendis officiis iusto voluptas! Quia, at cumque et voluptatum accusantium provident obcaecati maiores. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, at?
-            </p>
-          </div>
+        <div className='col-12 p-0'>
+          <Carousel slides={headerSlides} isHeader={true} />
         </div>
       </header>
       <section className='row main-section'>
@@ -178,31 +226,32 @@ function App() {
       </section>
       <section className='row testimonials-section'>
         <div className='col-12 p-0 d-flex flex-column'>
-          <h1 className='text-center py-3 m-0 mt-4 section-title'>Доволни потребители</h1>
+          <h1 className='text-center py-1 m-0 mt-4 section-title'>Доволни потребители</h1>
           <Carousel slides={slides} />
         </div>
       </section>
-      <footer className='row px-4 py-2'>
-        <div className="col-lg-4 pt-4">
+      <footer className='row p-4'>
+        <div className="col px-2">
           <div className="d-flex align-items-center">
-            <img src={Logo} width={150} className='rounded' />
-            <h1 className='ps-3'>Znanie</h1>
+            <h1>Znanie</h1>
           </div>
           <p className='pt-2'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse alias error nostrum nihil eligendi sit.
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi quo veritatis, omnis consectetur alias excepturi!
           </p>
         </div>
-        <div className="col-lg-8 pt-4 d-flex align-items-start">
-          <div>
-            <p className='d-flex align-items-center fs-3'><i className="fa-solid fa-location-dot fs-3" /><span className='ps-2'>Нашият Адрес</span></p>
-            <p>Address line 1</p>
-            <p>Address line 2</p>
+        <div className="col px-2">
+          <div className='footer-content'>
+            <p className='d-flex align-items-center justify-content-center fs-3 py-2 m-0 footer-box'><i className="fa-solid fa-location-dot fs-3" /><span className='ps-2'>Нашият Адрес</span></p>
+            <p className='px-2 pt-2'>Address line 1</p>
+            <p className='px-2 pb-2'>Address line 2</p>
           </div>
-          <div className='ps-4 ms-4'>
-            <p className='d-flex align-items-center fs-3'><i className="fa-solid fa-phone fs-3" /><span className='ps-2'>Контакти</span></p>
-            <p>+359 123 4567</p>
-            <p>+359 234 5678</p>
+        </div>
+        <div className="col px-2">
+          <div className='footer-content'>
+            <p className='d-flex align-items-center justify-content-center fs-3 py-2 m-0 footer-box'><i className="fa-solid fa-phone fs-3" /><span className='ps-2'>Контакти</span></p>
+            <p className='px-2 pt-2'>+359 123 4567</p>
+            <p className='px-2 pb-2'>+359 234 5678</p>
           </div>
         </div>
         <div className="col-12">
@@ -214,39 +263,62 @@ function App() {
   )
 }
 
-function Carousel({ slides }) {
+function Carousel({ slides, isHeader = false }) {
+  const id = useId();
+
   return (
-    <div id="carouselExampleIndicators" className="carousel slide flex-grow-1" data-bs-ride="carousel">
+    <div id={id} className="carousel slide h-100" data-bs-ride="carousel">
       <div className="carousel-indicators mb-1 mb-lg-3">
         {
-          slides.map((slide, i) => <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={i} className={`${i === 0 ? 'active' : ''}`} aria-current={`${i === 0}`} aria-label={`Slide ${i}`}></button>)
+          slides.map((slide, i) => <button type="button" data-bs-target={`#${id}`} data-bs-slide-to={i} className={`${i === 0 ? 'active' : ''}`} aria-current={`${i === 0}`} aria-label={`Slide ${i}`}></button>)
         }
       </div>
       <div className="carousel-inner h-100">
         {
-          slides.map((slide, i) => <CarouselSlide {...slide} active={i === 0} />)
+          slides.map((slide, i) => (isHeader) ? <HeaderSlide {...slide} active={i === 0} /> : <CarouselSlide {...slide} active={i === 0} />)
         }
       </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+      {
+        isHeader && (
+          <>
+            <button className="carousel-control-prev" type="button" data-bs-target={`#${id}`} data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target={`#${id}`} data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </>
+        )
+      }
     </div>
   );
 }
 
-function CarouselSlide({ name, comment, grade, active }) {
+function HeaderSlide({ header = 'Znanie', text, active, image }) {
+  return (
+    <div className={`carousel-item h-100 ${active ? 'active' : ''}`} data-bs-interval="5000">
+      <div className='w-100 h-100 d-flex align-items-center justify-content-center slide' style={{ backgroundImage: `url(${image})` }}>
+        <div className='col-6 slide-content p-4'>
+          <h1 className='text-center'>{header}</h1>
+          <p className='text-center'>
+            {text}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CarouselSlide({ image, name, comment, grade, active }) {
   return (
     <div className={`carousel-item h-100 ${active ? 'active' : ''}`} data-bs-interval="5000">
       <div className='w-100 h-100 d-flex align-items-center justify-content-center'>
         <div className='testimonial col-9 col-lg-6'>
-          <div className='d-flex align-items-center testimonial-top'>
-            <img src={Logo} />
-            <p className='ps-4 fs-3 m-0'>
+          <div className='d-flex flex-column align-items-center testimonial-top'>
+            <img src={image} />
+            <p className='d-flex flex-column align-items-center fs-3 m-0'>
               <span>
                 {name}
               </span>
@@ -255,7 +327,7 @@ function CarouselSlide({ name, comment, grade, active }) {
               </span>
             </p>
           </div>
-          <p className='testimonial-content fs-4'>"{comment}"</p>
+          <p className='testimonial-content text-center fs-4'>"{comment}"</p>
         </div>
       </div>
     </div>
