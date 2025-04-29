8a53c0254500e66f74b0ae43d2ed09a5a7f765d3
Tsurka <mirkova_@abv.bg>
2025-04-29 12:28:22 +0300

    Последни промени

--- src/pages/data/presentations.jsx
+++ src/pages/data/presentations.jsx
@@ -1,35 +1,37 @@
 const presentations = {
   "5-class": [
-    { id: 1, title: "Урок 1: Днешните български земи през праисторическата епоха", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQVG3hd7qMkxhnsJUTHQUhNzg6BQcMjkMhnHHepVy4G40aO-_ANxFT6Gx-_-i774v9lH4UGFoTnWKvG/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, link: "/History_files/presentation.files/5-class/Днешните_български_земи_през_праисторическата_епоха.pdf", external: true},
-    { id: 2, title: "Урок 2: Земеделието променя света", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQVG3hd7qMkxhnsJUTHQUhNzg6BQcMjkMhnHHepVy4G40aO-_ANxFT6Gx-_-i774v9lH4UGFoTnWKvG/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, link: "https://www.canva.com/design/DAGS5ESEYMM/BrzDDZRqDJM8KWaUIq7-BQ/edit", external: true },
+    { id: 1, title: "Урок 1: Днешните български земи през праисторическата епоха", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQVG3hd7qMkxhnsJUTHQUhNzg6BQcMjkMhnHHepVy4G40aO-_ANxFT6Gx-_-i774v9lH4UGFoTnWKvG/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, external: true},
+    { id: 2, title: "Урок 2: Земеделието променя света", 
+      element: (<iframe src="https://www.canva.com/design/DAGS5ESEYMM/fOFio-PJmb8foiloXq0pqA/view?embed" frameBorder="0" allowFullScreen={true} style={{ width: "100%", height: "100%", border: "none" }}/>), external: true},
   ],
-  "6-class": [
-    { id: 1, title: "Урок 1: Общество и всекидневие в България (кр. на XII - XIV в.)", link: "/History_files/presentation.files/6-class/Общество_и_всекидневие_в_България_(кр. на XII - XIV в.).pdf", external: true},
-    { id: 2, title: "Урок 2: Общество и всекидневие във Византия (кр. на XII - XIV в.)", link: "/courses/presentations/6"},
-  ],
+ "6-class": [
+  { id: 1, title: "Урок 1: Общество и всекидневие в България (кр. на XII - XIV в.)", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRk31cYJmea6sed3Di5s0m3x_ul8vTKHhuyPeA3BlwiaXn8McZC-umQWDr1sgTL_cucYJBMcQbCWV2b/pubembed?start=false&loop=false&delayms=3000" frameborder="0" width="100%" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, external: true },
+  { id: 2, title: "Урок 2: Общество и всекидневие във Византия (кр. на XII - XIV в.)", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSX3_YO7gE8qRtboFZeT744ncpXnTNHzyEokPN-npNLgOlxR1g3Yh5AsCD6U9T4U7Cs1P4vQZrIoGmm/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, external: true},
+],
+
   "7-class": [
-    { id: 1, title: "Урок 1: Въстанието на Асеневци и възстановяването на Българската държава", link: "/History_files/presentation.files/7-class/Асен_и_Петър.pdf", external: true },
-    { id: 2, title: "Урок 2: Българското националноосвободително движение (1869 – 1875 г.)", link: "/courses/presentations/7" },
+    { id: 1, title: "Урок 1: Въстанието на Асеневци и възстановяването на Българската държава", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSHwTapBNz6x313ASVg_KXabBO1zS4f3R4kZ8vEZZiG-Fmrn0xayphzLcDx9bdBcTBr50gf8O4tRTQb/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, external: true },
+    { id: 2, title: "Урок 2: Българското националноосвободително движение (1869 – 1875 г.)", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSX3_YO7gE8qRtboFZeT744ncpXnTNHzyEokPN-npNLgOlxR1g3Yh5AsCD6U9T4U7Cs1P4vQZrIoGmm/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
   ],
   "8-class": [
-    { id: 1, title: "Урок 1: Българската екзархия и търговските агенции в Македония и Одринско", link: "/History_files/presentation.files/8-class/Българска_Екзархия.pdf", external: true },
-    { id: 2, title: "Урок 2: Обществени движения и политически учения през първата половина на XIX век", link: "/courses/presentations/8" },
+    { id: 1, title: "Урок 1: Българската екзархия и търговските агенции в Македония и Одринско", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQCVxPTZHJagzPnipgoc9qixgLLYOPCWAFjIcaP-FEVFTwiQtlCSuUDJSSqEmO47i77wkgBdE4QEcTr/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, external: true },
+    { id: 2, title: "Урок 2: Обществени движения и политически учения през първата половина на XIX век", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRp0BFfRAcMYy-bcoPFQmgaTlDq78Er9-Xw6aMxRcazcBucE6K0uzjOcBhXpJDb_fANG0OsRNpsX0Ym/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />  },
   ],
   "9-class": [
     { id: 1, title: "Урок 1: България в Първата световна война 1914-1918 г.", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQIpteWEvrNkVxuqpIZMiWkeNxAlywjobMgjfpiSRUoUQfyjUTIrUjfkuKjloU3exWyRbbbjX9oMdJ4/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, link: "/History_files/presentation.files/9-class/УЧАСТИЕ_НА_БЪЛГАРИЯ_В ПЪРВАТА_СВЕТОВНА_ВОЙНА.pdf", external: true },
-    { id: 2, title: "Урок 2: Версайско-Вашингтонска система. Идеи за мир", link: "/courses/presentations/9" },
+    { id: 2, title: "Урок 2: Версайско-Вашингтонска система. Идеи за мир", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQUUPjEq7iYn1NT2dA9DS1_ItguAwMr1Ejz2avY3gUQFP5DSmtBZNjdUTiDR3vkAjxvjZr4V0m2cADA/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
   "10-class": [
     { id: 1, title: "Урок 1: Участие на българите в Първата световна война", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQIpteWEvrNkVxuqpIZMiWkeNxAlywjobMgjfpiSRUoUQfyjUTIrUjfkuKjloU3exWyRbbbjX9oMdJ4/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, link: "/History_files/presentation.files/10-class/УЧАСТИЕ_НА_БЪЛГАРИЯ_В ПЪРВАТА_СВЕТОВНА_ВОЙНА.pdf", external: true },
     { id: 2, title: "Урок 2: България в годините на Втората световна война", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQUUPjEq7iYn1NT2dA9DS1_ItguAwMr1Ejz2avY3gUQFP5DSmtBZNjdUTiDR3vkAjxvjZr4V0m2cADA/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, link: "/courses/presentations/10" },
   ],
   "11-class": [
-    { id: 1, title: "Урок 1: Християнизация на българите при княз Борис I (852-893 г.)", link: "/History_files/presentation.files/11-class/Християнизация_на_България.pdf", external: true },
-    { id: 2, title: "Урок 2: Политическото устройство на Дунавска България през VII-VIII в.", link: "/courses/presentations/11" },
+    { id: 1, title: "Урок 1: Християнизация на българите при княз Борис I (852-893 г.)", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQiZJ5AXUCTdC7uUaX4C0slpuKWBBHcio25C7lZ_PkiQsnNZWR97cPnHWCHbvbkKcjFtswT8TYYinub/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
+    { id: 2, title: "Урок 2: Политическото устройство на Дунавска България през VII-VIII в.", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTW3IHLdY9zzgaFG79hlU16uy6zsrOBSek91rHll0FXTDZVfotnTZmXMQeFiOscyp4PD2rZPGyYujpR/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
   "12-class": [
-    { id: 1, title: "Урок 1: Иконоборството във Византия (VIII–IX в.) - Йоан Дамаскин", link: "/History_files/presentation.files/12-class/Йоан_Дамаскин.pdf", external: true },
-    { id: 2, title: "Урок 2: Българската средновековна държава (VII-VIII в.) ", link: "/courses/presentations/12" },
+    { id: 1, title: "Урок 1: Иконоборството във Византия (VIII–IX в.) - Йоан Дамаскин", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR5ILBpgY48tz8CM1ZFDsKROK8Q8Pmktze27MJJDxMGF2tJF1C59FojnVKAJRyxGF0zso7D2fiFFcDJ/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, external: true },
+    { id: 2, title: "Урок 2: Българската средновековна държава (VII-VIII в.) ", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQQMqOyizMjR50XE6oz52VrVWZCTsqbS74T9D-ptzsQ6yQVP6qsV6sHsRqPmBAjqFHrY-pE36osJJ7m/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
 };
 
--- src/pages/data/presentations_IT.jsx
+++ src/pages/data/presentations_IT.jsx
@@ -1,35 +1,35 @@
 const presentations = {
   "5-class": [
-    { id: 1, title: "Урок 1: Въведение в информационните технологии и компютърните системи", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQktu--S2iF8lHbcQz8LMnSovORzw7s8EFDTLXXMIO6Zj43_HbvU7WoPBtPTh68RFBaNL6UfNMBeK21/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, link: "/IT_files/presentation.files/5-class/Компютърна_система_и_информационни_технологии.pdf", external: true },
-    { id: 2, title: "Урок 2: Диалог на потребителите с компютърни приложения", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQUUPjEq7iYn1NT2dA9DS1_ItguAwMr1Ejz2avY3gUQFP5DSmtBZNjdUTiDR3vkAjxvjZr4V0m2cADA/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />, link: "/it/presentations/5" },
+    { id: 1, title: "Урок 1: Въведение в информационните технологии и компютърните системи", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQktu--S2iF8lHbcQz8LMnSovORzw7s8EFDTLXXMIO6Zj43_HbvU7WoPBtPTh68RFBaNL6UfNMBeK21/pubembed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
+    { id: 2, title: "Урок 2: Диалог на потребителите с компютърни приложения", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSQMPjb2jyeN6NgGyyZzcGP3veUh9MRDxXgCwOrcJnlYLbT93SSFtO8MASRGZCwvN5VS-b-BWpG95zT/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
   ],
   "6-class": [
-    { id: 1, title: "Урок 1: Дигитализиране на изображение. Обработване и запазване", link: "/IT_files/presentation.files/6-class/Дигитализиране_на_изображение.pdf", external: true },
-    { id: 2, title: "Урок 2: Използване на звукови файлове и звукови ефекти", link: "/it/presentations/6" },
+    { id: 1, title: "Урок 1: Дигитализиране на изображение. Обработване и запазване", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSKqraFPwfe3aLwlV7BSfxP8XO-OUrhskdOK7SomeNUD9oVhkJRRzySgK9wGCn65TKcVe_r6hjerwAZ/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
+    { id: 2, title: "Урок 2: Използване на звукови файлове и звукови ефекти", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSi-qzvXzh5YC4BbClBbhC4ithBEr9b5vGLhJ7jJu4voerot4gxZ9m8jTZ16KLFctI8c_J5ce_p8qio/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
   "7-class": [
-    { id: 1, title: "Урок 1: Основни технически параметри на компютърната система", link: "/IT_files/presentation.files/7-class/Основни_технически_параметри_на_компютърната_система.pdf", external: true },
-    { id: 2, title: "Урок 2: Защита на информация. Използване на антивирусни програми", link: "/it/presentations/7" },
+    { id: 1, title: "Урок 1: Основни технически параметри на компютърната система", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR5MA3zUrkd9lGHB-BpePj8nQZUi5Zhp3crLzYpje284AIFKxlt1GHGCMfbePGC6Mt2bGiR7oRlWcUX/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
+    { id: 2, title: "Урок 2: Защита на информация. Използване на антивирусни програми", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSi-qzvXzh5YC4BbClBbhC4ithBEr9b5vGLhJ7jJu4voerot4gxZ9m8jTZ16KLFctI8c_J5ce_p8qio/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
   ],
   "8-class": [
-    { id: 1, title: "Урок 1: Създаване и интегриране на компонентите на уебсайт", link: "/IT_files/presentation.files/8-class/СЪЗДАВАНЕ_И_ИНТЕГРИРАНЕ_НА_КОМПОНЕНТИТЕ.pdf", external: true },
-    { id: 2, title: "Урок 2: Среди и средства за споделена съвместна работа и обучение", link: "/it/presentations/8" },
+    { id: 1, title: "Урок 1: Създаване и интегриране на компонентите на уебсайт", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSCaEKKQnPWD52T_pv00KcVCGjsDWZFLeCGQV1aNAH6botMb1EEiIyHVP4NtMQUNidzhTFeL_seMGy1/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
+    { id: 2, title: "Урок 2: Среди и средства за споделена съвместна работа и обучение", element:<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRJYIWglRYuKFwjrJLd8XcfaJHI4m4-BxK0asUzkXnjs_KsMpceSmmdSuBncduVEUlxpIUKGidlneKX/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
   "9-class": [
-    { id: 1, title: "Урок 1: Компютърни мрежи и услуги", link: "/IT_files/presentation.files/9-class/Компютърни_мрежи.pdf", external: true},
-    { id: 2, title: "Урок 2: Грид и облачни технологии", link: "/it/presentations/9" },
+    { id: 1, title: "Урок 1: Компютърни мрежи и услуги", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRZCxoP47OGIzXdviPAEq5uXJvNTEKxa_OjGCvXKtRMYviwqrsrKuDn67_zl6f_eZ4hHBhvCfQWOEVJ/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
+    { id: 2, title: "Урок 2: Грид и облачни технологии", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRqFtYy8v4RsfW4zjpXx2FtI2xSPgwf8zubzGZu2milpR5l4NeIksqoef4mVb3FThgqWx7l8_9wFEeq/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
   "10-class": [
-    { id: 1, title: "Урок 1: Начален преговор", link: "/IT_files/presentation.files/10-class/НАЧАЛЕН-ПРЕГОВОР.pdf", external: true },
-    { id: 2, title: "Урок 2: Етапи на разработване на БД", link: "/IT_files/presentation.files/10-class/ЕТАПИ_НА_РАЗРАБОТВАНЕ_НА_БД_1.pdf", external: true },
+    { id: 1, title: "Урок 1: Начален преговор", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRXEVc7vyUw18vVrHsTpxbQrp9oX2_PZz5pTrLpOgT-qACue4RZDJGfgKhqz6FDQSugIJY9EJfHiALZ/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
+    { id: 2, title: "Урок 2: Етапи на разработване на БД", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ8AdSPuDTPQeUp9S6gFKBe1gEUEr0I4CwYjnIbqXuikRvvNlwv3625vqxxkyFjW067EXNcgamjlULM/pubembed?start=false&loop=false&delayms=3000" frameborder="0" width="570" height="350" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
   "11-class": [
-    { id: 1, title: "Урок 1: Цифрово заснемане. Фотоапарат и видеокамера", link: "/IT_files/presentation.files/11-class/Цифрово_заснемане-основно_запознаване.pdf", external: true },
-    { id: 2, title: "Урок 2: Цифрово заснемане. основни настройки", link: "/it/presentations/11" },
+    { id: 1, title: "Урок 1: Цифрово заснемане. Фотоапарат и видеокамера", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSXVCIyjKCPfdSDaZrosJ6sGExV7N-cAKWhuUcAG1pP9pqgTAhMUjNOkvUsAmJ8pvHFnncA4pEZ1cZD/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
+    { id: 2, title: "Урок 2: Цифрово заснемане. основни настройки", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQbXOY5YyMKs88lKBwUOJm6apxm5aS1_vxTFHtls9CGheINqKqow8dVhpEIoVkHS-bvJnjXnhAa4k3M/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
   ],
   "12-class": [
-    { id: 1, title: "Урок 1: Основни етапи в планирането на уеб сайт", link: "/IT_files/presentation.files/12-class/ОСНОВНИ_ЕТАПИ_В_ПЛАНИРАНЕТО_НА_УЕБ_САЙТ.pdf", external: true },
-    { id: 2, title: "Урок 2: Избор на име и регистриране на уеб сайт", link: "/it/presentations/12" },
+    { id: 1, title: "Урок 1: Създаване на графичен модел на уеб страница", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTAlPfMpPpgYDhe6iV1GlpLj8wnAkdXa3WSEyZHUOTjfYdCPQbm8m4iGBGheB0yDXyEsUX7j2662DbD/pubembed?start=false&loop=false&delayms=3000" frameborder="0" width="570" height="350" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" /> },
+    { id: 2, title: "Урок 2: Избор на име и регистриране на уеб сайт", element: <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRB1dRXaEfvV7_YiP4tycTsmxrpwj5GVuPwb7YGl3CPZz-1h7SIEbM8swPuj99Kq8L7dmcVi3pItnkV/pubembed?start=false&loop=false&delayms=3000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />},
   ],
 };
 
